f610cfdaaa04ad609a3cdf966abca5a1d4f87bf2
f2reninj5 <16m.nowak@myton.co.uk>
2021-10-01 17:09:41 +0100

    Implemented base profile command and view subcommand

---
+++ commands/user/profile/profile.js
@@ -0,0 +1,88 @@
+const oggbot = require(`${__root}/oggbot`)
+const Discord = require('discord.js')
+const path = require('path')
+
+module.exports = {
+
+    data: {
+
+        name: 'profile',
+        description: 'Manage or view user profiles.',
+        type: 1,
+        options: [
+
+            {
+                name: 'view',
+                description: 'View a user profile.',
+                type: 1,
+                options: [
+
+                    {
+                        name: 'user',
+                        description: 'The user whose profile to show.',
+                        type: 6
+                    }
+                ]
+            },
+            {
+                name: 'set',
+                description: 'Manage your profile.',
+                type: 1,
+                options: [
+
+                    {
+                        name: 'title',
+                        description: 'Set your title.',
+                        type: 3
+                    },
+                    {
+                        name: 'location',
+                        description: 'Set your location.',
+                        type: 3
+                    },
+                    {
+                        name: 'description',
+                        description: 'Set your description.',
+                        type: 3
+                    },
+                    {
+                        name: 'style',
+                        description: 'Set your profile style.',
+                        type: 3
+                    }
+                ]
+            }
+        ]
+    },
+    async execute(interaction) {
+
+        const subcommands = {
+
+            none: new Discord.Collection()
+        }
+
+        oggbot.loadSubcommands(subcommands, path.resolve(__dirname, './subcommands'), true)
+
+        let group
+        
+        try {
+
+            group = interaction.options.getSubcommandGroup()
+
+        } catch {
+
+            group = 'none'
+        }
+        
+        let subcommand = subcommands[group].get(interaction.options.getSubcommand())
+
+        try {
+
+            subcommand.execute(interaction)
+
+        } catch {
+
+            interaction.editReply({ content: 'Phat error ennit.' })
+        }
+    }
+}
